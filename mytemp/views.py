b738b36012ff2ca44105f83f171fa220b9ec7712
武明辉 <1032662429@qq.com>
2017-02-20 17:41:32 +0800

    load

--- mytemp/views.py
+++ mytemp/views.py
@@ -1,3 +1,11 @@
+#-*-coding:utf-8-*-
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    #基本元素切换导入
+    return render(request, 'mytemp/mainpage.html')
+
+def index2(request):
+    return render(request, 'mytemp/mainpage2.html')
